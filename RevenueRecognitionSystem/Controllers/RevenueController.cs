4fcc1477e2d5de9586f57b44035caa04f68020c4
JakubBankowski <s33203@pjwstk.edu.pl>
2026-06-13 19:21:38 +0200

    Added revenue controller

---
+++ RevenueRecognitionSystem/Controllers/RevenueController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RevenueRecognitionSystem.Data;
+
+namespace RevenueRecognitionSystem.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/revenue")]
+public class RevenueController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    
+    public RevenueController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("current")]
+    public async Task<IActionResult> GetCurrentRevenue([FromQuery] int? productId)
+    {
+        var query = _context.Contracts.Where(c => c.IsPaid == true);
+
+        if (productId.HasValue) query = query.Where(c => c.SoftwareId == productId.Value);
+
+        decimal total = await query.SumAsync(c => c.TotalPrice);
+        return Ok(new{Revenue = total, Type = "Current Recognized Revenue"});
+    }
+
+    public async Task<IActionResult> GetPredictedRevenue([FromQuery] int? productId)
+    {
+        DateTime now = DateTime.UtcNow;
+
+        var query = _context.Contracts.Where(c => c.IsPaid == true || (c.IsPaid == false && now <= c.EndDate));
+
+        if (productId.HasValue)
+        {
+            query = query.Where(c=> c.SoftwareId == productId.Value);
+        }
+
+        decimal predictedRevenue = await query.SumAsync(c => c.TotalPrice);
+        return Ok(new {Revenue = predictedRevenue, Type = "Predicted Potential Revenue"});
+    }
+}
